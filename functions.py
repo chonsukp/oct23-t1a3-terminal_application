5db3934091ddb72bcb4b1cb27c76b03e18ff5266
chonsukp <chonsukp@gmail.com>
2023-12-17 18:37:27 +1000

    Refactor Welcome menu

--- functions.py
+++ functions.py
@@ -1,22 +1,15 @@
 import csv
-import os
-import getpass
 
-ascii_art = """
-            _____  __  _ 
-            |__/ \/ |\ | TM
-            |  \_/\_| \|
 
-"""
-def welcome():
-    print(ascii_art)
+# Function - Check if user exists
+def user_authenticate():
+    print("Authenticate user")
 
-# Function - Main Menu
-def main_menu():
-    print("Welcome to RXN! Your personal running journal.\n")
-    print(ascii_art)
-    print("Please choose the following options:\n")
-    print("1. Login")
-    print("2. Register")
-    print("3. Exit")
+# Function - Register new user
+def user_register():
+    print("Register user")
+
+# Function - Login existing user
+def user_login():
+    print("Login user")
 
--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-import functions 
+from functions import user_authenticate, user_register, user_login
 
-def main():
-    functions.welcome()
+ascii_art = """
+_____  __  _ 
+|__/ \/ |\ | 
+|  \_/\_| \|
 
-    functions.main_menu()
-main()
+"""
+print(ascii_art)
+
+# Function - Welcome Menu - Prompting user to login or register.
+def welcome_menu():
+    print("Welcome to RXN! Your personal running journal.\n")
+    print("Please choose the following options:")
+    print("1. Login")
+    print("2. Register")
+    print("3. Exit\n")
+    decision = input("Enter an option (1/2/3): ")
+    return decision
+
+user_decision = ""
+
+while user_decision != "3":
+    user_decision = welcome_menu()
+    if (user_decision == "1"):
+        user_login()
+    elif (user_decision == "2"):
+        user_register()
+    elif (user_decision == "3"):
+        continue
+    else:
+        print("Invalid Input. Please Try Again")
+
+print("Thank you for using RXN!")
+
+
+
+
+# def main_menu():
+#     print("What would you like to do:")
+#     print("1. Log a run")
+#     print("2. Display logged runs")
+#     print("3. Edit run")
+#     print("4. Delete run")
+#     print("5. Exit")
