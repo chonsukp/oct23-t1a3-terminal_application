10197c83a2154bd52b98f3ce3e5732060521928f
chonsukp <chonsukp@gmail.com>
2023-12-18 22:22:26 +1000

    Implement Edit function

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import tabulate
 
 # Feature 1 - Log a run
 def log_run(runs_file):
+    # Display feature header
     print(f"\n{fg('black')}{bg('white')}Log runs:{attr('reset')}\n")
 
     # Input - title of the run
@@ -20,24 +21,13 @@ def log_run(runs_file):
             continue
         else:
             break
-
-    # Input - time of the run 
-    while True:
-        try:
-            time_str = input("Enter the time of the run in 24-Hour format (HH:MM): ")
-            time = datetime.datetime.strptime(time_str, "%H:%M").time()
-        except ValueError:
-            print("Invalid time format. Please enter time in HH:MM format.")
-            continue
-        else:
-            break
     
     # Input - distance of the run
     while True:
         try:
             distance = float(input("Enter the distance of the run in kilometers: "))
             if distance < 0:
-                print("Invalid distance value. Please enter a non-negative number.")
+                print("Invalid distance value. Please enter a whole number.")
                 continue
         except ValueError:
             print("Invalid input. Please enter a valid distance in number only.")
@@ -59,9 +49,10 @@ def log_run(runs_file):
     # Input - notes for the run
     notes = input("Enter notes for the run: ")
 
+    # Append run inputs to CSV file
     with open(runs_file, "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow([title, date_str, time_str, distance, time_taken_str, notes])
+        writer.writerow([title, date, distance, time_taken, notes])
 
 # Function to convert time to seconds
 def convert_time_to_seconds(time_str):
@@ -70,19 +61,23 @@ def convert_time_to_seconds(time_str):
 
 # Feature 2 - View Logs
 def view_log(runs_file):
+    # Display feature header
     print(f"\n{fg('black')}{bg('white')}View runs:{attr('reset')}\n")
 
+    # Read data from CSV file
     with open(runs_file, "r") as f:
         csv_reader = csv.reader(f)
         headers = next(csv_reader)
         data = list(csv_reader)
 
+    # Convert data to a list of lists
     data_as_lists = [list(map(str, row)) for row in data]
 
+    # Calculate total runs, total distance, total time taken and average pace
     total_runs = len(data_as_lists)
-    total_distance = sum(float(row[3]) for row in data_as_lists)
+    total_distance = sum(float(row[2]) for row in data_as_lists)
 
-    total_time_taken_seconds = sum(convert_time_to_seconds(row[4]) for row in data_as_lists)
+    total_time_taken_seconds = sum(convert_time_to_seconds(row[3]) for row in data_as_lists)
 
     total_time_hours = total_time_taken_seconds // 3600
     total_time_minutes = (total_time_taken_seconds % 3600) // 60
@@ -91,10 +86,86 @@ def view_log(runs_file):
     average_pace_seconds_per_km = total_time_taken_seconds / total_distance
     average_pace_minutes_per_km = average_pace_seconds_per_km / 60
 
+    # Create a table using tabulate module 
     table = tabulate.tabulate(data_as_lists, headers, tablefmt='pretty')
 
+    # Display the table and run stats
     print(table)
     print(f"\nTotal runs: {total_runs}.")
     print(f"Total distance: {total_distance} kilometers.")
     print(f"Total time: {total_time_hours} hours, {total_time_minutes} minutes, {total_time_seconds} seconds.")
-    print(f"Average pace: {average_pace_minutes_per_km:.2f} minutes/kilometers.\n")
+    print(f"Average pace: {average_pace_minutes_per_km:.2f} minutes/kilometers.")
+
+# Feature 3 - Edit log
+def edit_log(runs_file):
+    print(f"\n{fg('black')}{bg('white')}Edit runs:{attr('reset')}\n")
+
+    # Load existing data from CSV file
+    with open(runs_file, "r") as f:
+        csv_reader = csv.reader(f)
+        headers = next(csv_reader)
+        data = list(csv_reader)
+
+    # Convert data to a list of list
+    data_as_lists = [list(map(str, row)) for row in data]
+
+    # Display the existing logs
+    table = tabulate.tabulate(data_as_lists, headers, tablefmt='pretty')
+    print(table)
+
+    # Input - ask user which log to edit
+    while True:
+        try:
+            row_to_edit = int(input("\nEnter the row number to edit: "))
+            if 0 <= row_to_edit < len(data_as_lists):
+                break
+            else:
+                print("Invalid row number. Please enter a valid row number.")
+        except ValueError:
+            print("Invalid input. Please enter a valid row number.")
+
+    # Input - ask user for new log inputs
+    title = input("Enter a new title for the run: ")
+
+    while True:
+        try:
+            date_str = input("Enter the new date of the run (DD/MM/YYYY): ")
+            date = datetime.datetime.strptime(date_str, "%d/%m/%Y").date()
+        except ValueError:
+            print("Invalid date format. Please enter date in DD/MM/YYYY format.")
+            continue
+        else:
+            break
+
+    while True:
+            try:
+                distance = float(input("Enter the new distance of the run in kilometers: "))
+                if distance < 0:
+                    print("Invalid distance value. Please enter a whole number.")
+                    continue
+            except ValueError:
+                print("Invalid input. Please enter a valid distance in number only.")
+                continue
+            else:
+                break
+
+    while True:
+            try:
+                time_taken_str = input("Enter the new time taken for the run (HH:MM:SS): ")
+                time_taken = datetime.datetime.strptime(time_taken_str, "%H:%M:%S").time()
+            except ValueError:
+                print("Invalid input. Please enter a valid time in HH:MM:SS format.")
+                continue
+            else:
+                break
+
+    notes = input("Enter new notes for the run: ")
+
+    # Update the selected row
+    data_as_lists[row_to_edit] = [title, date, distance, time_taken, notes]
+
+    # Save the updated data
+    with open(runs_file, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(data_as_lists)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from colored import fg, attr, bg
 from tabulate import tabulate
-from functions import log_run, view_log
+from functions import log_run, view_log, edit_log
 
 runs_file = "runs.csv"
 
@@ -10,7 +10,7 @@ try:
 
 except FileNotFoundError:
     rxn_file = open(runs_file, "w")
-    rxn_file.write("TITLE,DATE,TIME,DISTANCE,TIME TAKEN,NOTES\n")
+    rxn_file.write("TITLE,DATE,DISTANCE,TIME TAKEN,NOTES\n")
     rxn_file.close()
 
 def main():
@@ -25,12 +25,12 @@ def main():
 
     # Function - Welcome Menu - Prompting user to login or register.
     def welcome_menu():
-        print(f"{fg('black')}{bg('white')}Welcome to RXN! Your personal running journal.{attr('reset')}\n")
+        print(f"\n{fg('black')}{bg('white')}Welcome to RXN! Your personal running journal.{attr('reset')}\n")
         print("Please choose the following options:")
-        print("1. Log a run")
-        print("2. View all runs")
-        print("3. Edit a run")
-        print("4. Remove a run")
+        print("1. Log run")
+        print("2. View log")
+        print("3. Edit log")
+        print("4. Remove log")
         print("5. Exit\n")
         decision = input("Enter an option (1-5): ")
         return decision
@@ -44,8 +44,7 @@ def main():
         elif (user_decision == "2"):
             view_log(runs_file)
         elif (user_decision == "3"):
-            # Edit_run(runs_file)
-            print("Three")
+            edit_log(runs_file)
         elif (user_decision == "4"):
             # remove_run(runs_file)
             print("Four")
